0b42d8e051b066789087970048c509a8a73b9fe3
golubqv <GolubYanndex@yandex.ru>
2026-07-10 20:48:53 +0300

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,3 +1,5 @@
+// Главная страница
+
 const startBtn = document.getElementById("startBtn");
 
 if (startBtn) {
@@ -10,13 +12,55 @@ if (startBtn) {
 
 }
 
+// Регистрация
+
+const registerBtn = document.getElementById("registerBtn");
+
+if (registerBtn) {
+
+    registerBtn.onclick = () => {
+
+        const username = document.getElementById("username").value;
+
+        const email = document.getElementById("email").value;
+
+        const password = document.getElementById("password").value;
+
+        if (
+            username === "" ||
+            email === "" ||
+            password === ""
+        ) {
+
+            alert("Заполните все поля.");
+
+            return;
+
+        }
+
+        localStorage.setItem("tg_username", username);
+
+        localStorage.setItem("tg_email", email);
+
+        localStorage.setItem("tg_password", password);
+
+        alert("Аккаунт создан!");
+
+        location.href = "login.html";
+
+    };
+
+}
+
+// Вход
+
 const loginBtn = document.getElementById("loginBtn");
 
 if (loginBtn) {
 
     loginBtn.onclick = () => {
 
-        alert("Авторизация скоро будет подключена.");
+        alert("Сервер скоро будет подключён.");
 
     };
 
