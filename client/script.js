41231fd230f8464f4ebba9849d4d9444eff9732f
golubqv <GolubYanndex@yandex.ru>
2026-07-10 20:40:27 +0300

    Create script.js

---
+++ client/script.js
@@ -0,0 +1,17 @@
+```javascript
+document
+.getElementById("login")
+.onclick = () => {
+
+    alert("Страница входа скоро появится.");
+
+}
+
+document
+.getElementById("register")
+.onclick = () => {
+
+    alert("Страница регистрации скоро появится.");
+
+}
+```
