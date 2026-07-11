79dbf3ee38969d10d96ead558b2705fe1db8bfcb
golubqv <GolubYanndex@yandex.ru>
2026-07-11 19:41:29 +0300

    Update chatService.js

--- client/js/chatService.js
+++ client/js/chatService.js
@@ -46,4 +46,40 @@ class ChatService {
 
     }
 
+        delete(id) {
+
+        this.chats = this.chats.filter(chat => chat.id !== id);
+
+        this.save();
+
+    }
+
+    update(id, data) {
+
+        const chat = this.chats.find(chat => chat.id === id);
+
+        if (!chat) return;
+
+        Object.assign(chat, data);
+
+        this.save();
+
+    }
+
+    get(id) {
+
+        return this.chats.find(chat => chat.id === id);
+
+    }
+
+    save() {
+
+        db.save(db.keys.chats, this.chats);
+
+    }
+
+}
+
+const chatService = new ChatService();
+
    
